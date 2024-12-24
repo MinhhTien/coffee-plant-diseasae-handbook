a13cbcb1752b6016616a00a2d7b5f5b855f46e6e
Martin Vo <92145479+MinhhTien@users.noreply.github.com>
2024-12-24 12:44:50 +0700

    init: template UI

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Cẩm nang số bệnh cây cà phê",
+  description: "Xây dựng cẩm nang số bệnh cây cà phê ở Việt Nam",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
+}
---
+++ src/components/Charts/ChartThree.tsx
@@ -0,0 +1,121 @@
+import { ApexOptions } from "apexcharts";
+import React from "react";
+import ReactApexChart from "react-apexcharts";
+import DefaultSelectOption from "@/components/SelectOption/DefaultSelectOption";
+
+const ChartThree: React.FC = () => {
+  const series = [65, 34, 68];
+
+  const options: ApexOptions = {
+    chart: {
+      fontFamily: "Roboto, sans-serif",
+      type: "donut",
+    },
+    colors: ["#5750F1", "#5475E5", "#8099EC", "#ADBCF2"],
+    labels: ["Desktop", "Tablet", "Mobile", "Unknown"],
+    legend: {
+      show: false,
+      position: "bottom",
+    },
+
+    plotOptions: {
+      pie: {
+        donut: {
+          size: "80%",
+          background: "transparent",
+          labels: {
+            show: true,
+            total: {
+              show: true,
+              showAlways: true,
+              label: "Triệu chứng",
+              fontSize: "16px",
+              fontWeight: "400",
+            },
+            value: {
+              show: true,
+              fontSize: "28px",
+              fontWeight: "bold",
+            },
+          },
+        },
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    responsive: [
+      {
+        breakpoint: 2600,
+        options: {
+          chart: {
+            width: 415,
+          },
+        },
+      },
+      {
+        breakpoint: 640,
+        options: {
+          chart: {
+            width: 200,
+          },
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className="col-span-12 rounded-[10px] bg-white px-7.5 pb-7 pt-7.5 shadow-1 dark:bg-gray-dark dark:shadow-card xl:col-span-5">
+      <div className="mb-9 justify-between gap-4 sm:flex">
+        <div>
+          <h4 className="text-body-2xlg font-bold text-dark dark:text-white">
+            Mức độ nghiêm trọng
+          </h4>
+        </div>
+        <div>
+          <DefaultSelectOption options={["Yearly", "Monthly" ]} />
+        </div>
+      </div>
+
+      <div className="mb-8">
+        <div className="mx-auto flex justify-center">
+          <ReactApexChart options={options} series={series} type="donut" />
+        </div>
+      </div>
+
+      <div className="mx-auto w-full max-w-[350px]">
+        <div className="-mx-7.5 flex flex-wrap items-center justify-center gap-y-2.5">
+          <div className="w-full px-7.5 sm:w-1/2">
+            <div className="flex w-full items-center">
+              <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-blue"></span>
+              <p className="flex w-full justify-between text-body-sm font-medium text-dark dark:text-dark-6">
+                <span> Nghiêm trọng </span>
+                <span> 65% </span>
+              </p>
+            </div>
+          </div>
+          <div className="w-full px-7.5 sm:w-1/2">
+            <div className="flex w-full items-center">
+              <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-blue-light"></span>
+              <p className="flex w-full justify-between text-body-sm font-medium text-dark dark:text-dark-6">
+                <span> Trunng bình </span>
+                <span> 34% </span>
+              </p>
+            </div>
+          </div>
+          <div className="w-full px-7.5 sm:w-1/2">
+            <div className="flex w-full items-center">
+              <span className="mr-2 block h-3 w-full max-w-3 rounded-full bg-blue-light-3"></span>
+              <p className="flex w-full justify-between text-body-sm font-medium text-dark dark:text-dark-6">
+                <span> Nhẹ </span>
+                <span> 57% </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartThree;
